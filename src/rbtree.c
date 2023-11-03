095351561c8130ef0734ff4db9395256f7299624
Classbinu <classbinu@gmail.com>
2023-11-03 20:46:02 +0900

    트리 초기화, 삭제 구현

--- src/rbtree.c
+++ src/rbtree.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 rbtree *new_rbtree(void) {
-  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
+  rbtree *t = (rbtree *)calloc(1, sizeof(rbtree));
   // TODO: initialize struct if needed
-  if (!p) {
+  if (!t) {
     printf("메모리 할당에 실패하였습니다.\n");
     return NULL;
   }
@@ -14,7 +14,7 @@ rbtree *new_rbtree(void) {
   node_t *nil_node = (node_t*)calloc(1, sizeof(node_t));
   if (!nil_node) {
     printf("메모리 할당에 실패하였습니다.\n");
-    free(p);
+    free(t);
     return NULL;
   }
 
@@ -25,15 +25,53 @@ rbtree *new_rbtree(void) {
   nil_node->right = nil_node;
 
   // 트리의 멤버를 설정합니다.
-  p->root = NULL;
-  p->nil = nil_node;
+  t->root = nil_node;
+  t->nil = nil_node;
 
-  printf("트리를 생성하였습니다.\n");
-  return p;
+  return t;
 }
 
 void delete_rbtree(rbtree *t) {
   // TODO: reclaim the tree nodes's memory
+  if (!t) return;
+
+  // 스택 공간을 할당합니다.
+  node_t *stack[1024];
+  int stack_top = 0;
+
+  // 스택에 루트 노드를 넣고 스택 포인터를 +1합니다.
+  if (t->root != t->nil) {
+    stack[stack_top] = t->root;
+    stack_top++;
+  }
+
+  // 스택이 비어있을 때까지 반복합니다.
+  while (stack_top > 0) {
+
+    // 스택에서 노드를 pop합니다.
+    stack_top--;
+    node_t *node = stack[stack_top];
+
+    // NIL노드가 아니면 왼쪽 노드를 스택에 push합니다.
+    if (node->left != t->nil) {
+      stack[stack_top] = node->left;
+      stack_top++;
+    }
+
+    // NIL노드가 아니면 오른쪽 노드를 스택에 push합니다.
+    if (node->right != t->nil) {
+      stack[stack_top] = node->right;
+      stack_top++;
+    }
+
+    // 꺼낸 노드를 해제합니다.
+    free(node);
+  }
+
+  // 모든 노드를 해제 후에 마지막으로 NIL노드를 해제합니다.
+  free(t->nil);
+
+  // 트리를 해제합니다.
   free(t);
 }
 
