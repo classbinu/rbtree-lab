5859ca9918cf765780c2fbe27daf837594419549
Classbinu <classbinu@gmail.com>
2023-11-04 22:13:32 +0900

    삽입 코드 테스트 완료

--- src/rbtree.c
+++ src/rbtree.c
@@ -2,18 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-rbtree *new_rbtree(void) {
+void print_malloc_failed() { printf("메모리 할당에 실패하였습니다.\n"); }
+
+rbtree *new_rbtree(void)
+{
   rbtree *t = (rbtree *)calloc(1, sizeof(rbtree));
 
   if (!t) {
-    printf("메모리 할당에 실패하였습니다.\n");
+    print_malloc_failed();
     return NULL;
   }
 
   // T.nil을 생성합니다. 생성된 T.nil은 트리 내에서 계속 사용됩니다.
-  node_t *nil_node = (node_t*)calloc(1, sizeof(node_t));
+  node_t *nil_node = (node_t *)calloc(1, sizeof(node_t));
   if (!nil_node) {
-    printf("메모리 할당에 실패하였습니다.\n");
+    print_malloc_failed();
     free(t);
     return NULL;
   }
@@ -31,9 +34,11 @@ rbtree *new_rbtree(void) {
   return t;
 }
 
-void delete_rbtree(rbtree *t) {
+void delete_rbtree(rbtree *t)
+{
 
-  if (!t) return;
+  if (!t)
+    return;
 
   // 스택 공간을 할당합니다.
   node_t *stack[1024];
@@ -78,88 +83,192 @@ void delete_rbtree(rbtree *t) {
 }
 
 // 좌회전 함수
-void left_rotate(rbtree *t, node_t *x) {
-    if (x == t->nil || x->right == t->nil) {
-      return;
-    }
+void left_rotate(rbtree *t, node_t *x)
+{
+  if (x == t->nil || x->right == t->nil) {
+    return;
+  }
 
-    // y를 설정
-    node_t *y = x->right;
+  // y를 설정
+  node_t *y = x->right;
 
-    // y의 왼쪽 서브트리를 x의 오른쪽 서브트리로 옮긴다.
-    x->right = y->left;
-    y->left->parent = x;
+  // y의 왼쪽 서브트리를 x의 오른쪽 서브트리로 옮긴다.
+  x->right = y->left;
+  y->left->parent = x;
 
-    // y의 부모를 x의 부모로 변경한다.(y를 부모 자리로 승격)
-    y->parent = x->parent;
-    if (x->parent == t->nil) {  // x가 루트였다면 승격된 y를 트리의 루트로 설정
-      t->root = y;
-    } else if (x == x->parent->left) {  // x가 왼쪽 자식 노드였다면 승격된 y를 기존 부모의 왼쪽 자식으로 설정
-      x->parent->left = y;
-    } else {  // // x가 오른쪽 자식 노드였다면 승격된 y를 기존 부모의 오른쪽 자식으로 설정
-      x->parent->right = y;
-    }
+  // y의 부모를 x의 부모로 변경한다.(y를 부모 자리로 승격)
+  y->parent = x->parent;
+  if (x->parent == t->nil) { // x가 루트였다면 승격된 y를 트리의 루트로 설정
+    t->root = y;
+  } else if (x == x->parent->left) { // x가 왼쪽 자식 노드였다면 승격된 y를 기존
+                                     // 부모의 왼쪽 자식으로 설정
+    x->parent->left = y;
+  } else { // // x가 오른쪽 자식 노드였다면 승격된 y를 기존 부모의 오른쪽
+           // 자식으로
+    // 설정
+    x->parent->right = y;
+  }
 
-    // 승격된 y와 강등된 x의 관계를 설정
-    y->left = x;
-    x->parent = y;
+  // 승격된 y와 강등된 x의 관계를 설정
+  y->left = x;
+  x->parent = y;
 }
-
 
 // 우회전 함수
-void right_rotate(rbtree *t, node_t *x) {
-      if (x == t->nil || x->left == t->nil) {
-      return;
+void right_rotate(rbtree *t, node_t *x)
+{
+  if (x == t->nil || x->left == t->nil) {
+    return;
+  }
+
+  // y를 설정
+  node_t *y = x->left;
+
+  // y의 오른쪽 서브트리를 x의 왼쪽 서브트리로 옮긴다.
+  x->left = y->right;
+  y->right->parent = x;
+
+  // y의 부모를 x의 부모로 변경한다.(y를 부모 자리로 승격)
+  y->parent = x->parent;
+  if (x->parent == t->nil) { // x가 루트였다면 승격된 y를 트리의 루트로 설정
+    t->root = y;
+  } else if (x == x->parent->left) { // x가 왼쪽 자식 노드였다면 승격된 y를 기존
+                                     // 부모의 왼쪽 자식으로 설정
+    x->parent->left = y;
+  } else { // // x가 오른쪽 자식 노드였다면 승격된 y를 기존 부모의 오른쪽
+           // 자식으로
+    // 설정
+    x->parent->right = y;
+  }
+
+  // 승격된 y와 강등된 x의 관계를 설정
+  y->left = x;
+  x->parent = y;
+}
+
+void rbtree_insert_fixup(rbtree *t, node_t *z)
+{
+  // 신규 노드가 루트면 끝 && 신규 노드의 부모 레드면 계속 체크
+  while (z != t->root && z->parent->color == RBTREE_RED) {
+    if (z->parent == z->parent->parent->left) { // 부모가 왼쪽 자식이라면
+      node_t *y = z->parent->parent->right;     // 삼촌 노드 설정
+      if (y->color == RBTREE_RED) {
+        // 케이스 1: z의 삼촌 y가 레드
+        z->parent->color = RBTREE_BLACK; // 부모 노드를 블랙으로 설정
+        y->color = RBTREE_BLACK;         // 삼촌 노드를 블랙으로 설정
+        z->parent->parent->color =
+            RBTREE_RED; // 조부모를 레드로 설정(부모와 삼촌에게 블랙을 물려줌)
+        z = z->parent->parent; // 레드로 설정된 조부모를 신규 삽입 노드로 취급
+      } else {
+        if (z == z->parent->right) {
+          // 케이스 2: z가 오른쪽 자식인 경우(케이스 2는 궁극적으로 케이스 3이
+          // 됨)
+          z = z->parent; // 회전 기준을 z의 부모로 설정
+          left_rotate(t, z);
+        }
+        // 케이스 3: z가 왼쪽 자식인 경우
+        z->parent->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        right_rotate(t, z->parent->parent); // 조부모를 기준으로 회전
+      }
+    } else { // 부모가 오른쪽 자식이라면(좌회전과 반대 코드)
+      node_t *y = z->parent->parent->left;
+      if (y->color == RBTREE_RED) {
+        z->parent->color = RBTREE_BLACK;
+        y->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        z = z->parent->parent;
+      } else {
+        if (z == z->parent->left) {
+          z = z->parent;
+          right_rotate(t, z);
+        }
+        z->parent->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        left_rotate(t, z->parent->parent);
+      }
     }
+  }
+  t->root->color = RBTREE_BLACK;
+}
 
-    // y를 설정
-    node_t *y = x->left;
+node_t *rbtree_insert(rbtree *t, const key_t key)
+{
+  node_t *new_node = (node_t *)(malloc(sizeof(node_t)));
+  if (new_node == NULL) {
+    print_malloc_failed();
+    return t->root;
+  }
 
-    // y의 오른쪽 서브트리를 x의 왼쪽 서브트리로 옮긴다.
-    x->left = y->right;
-    y->right->parent = x;
+  new_node->key = key;
+  new_node->color = RBTREE_RED;
+  new_node->left = t->nil;
+  new_node->right = t->nil;
 
-    // y의 부모를 x의 부모로 변경한다.(y를 부모 자리로 승격)
-    y->parent = x->parent;
-    if (x->parent == t->nil) {  // x가 루트였다면 승격된 y를 트리의 루트로 설정
-      t->root = y;
-    } else if (x == x->parent->left) {  // x가 왼쪽 자식 노드였다면 승격된 y를 기존 부모의 왼쪽 자식으로 설정
-      x->parent->left = y;
-    } else {  // // x가 오른쪽 자식 노드였다면 승격된 y를 기존 부모의 오른쪽 자식으로 설정
-      x->parent->right = y;
+  node_t *current = t->root;
+  node_t *parent = t->nil;
+
+  // BST에 따라 루트부터 신규 노드가 삽입될 위치를 찾습니다.
+  while (current != t->nil) {
+    parent = current;
+    if (key < current->key) {
+      current = current->left;
+    } else if (key > current->key) {
+      current = current->right;
+    } else {
+      // 키가 중복되었을 경우
+      printf("키가 중복되었습니다.\n");
+      free(new_node);
+      new_node = NULL;
+      return t->root;
     }
+  }
 
-    // 승격된 y와 강등된 x의 관계를 설정
-    y->left = x;
-    x->parent = y;
-}
+  // 신규 노드의 부모를 설정
+  new_node->parent = current;
 
-node_t *rbtree_insert(rbtree *t, const key_t key) {
-  // TODO: implement insert
+  if (parent == t->nil) { // 부모가 nil이면 신규 노드를 트리의 루트로 설정
+    t->root = new_node;
+  } else if (key < parent->key) { // 신규 노드의 키가 부모의 키보다 작으면
+                                  // 부모의 왼쪽 자식으로 설정
+    parent->left = new_node;
+  } else {
+    parent->right = new_node; // 신규 노드의 키가 부모의 키보다 작으면
+                              // 부모의 오른쪽 자식으로 설정
+  }
+
+  // RB Tree 특성 복구
+  rbtree_insert_fixup(t, new_node);
+
   return t->root;
 }
 
-node_t *rbtree_find(const rbtree *t, const key_t key) {
+node_t *rbtree_find(const rbtree *t, const key_t key)
+{
   // TODO: implement find
   return t->root;
 }
 
-node_t *rbtree_min(const rbtree *t) {
+node_t *rbtree_min(const rbtree *t)
+{
   // TODO: implement find
   return t->root;
 }
 
-node_t *rbtree_max(const rbtree *t) {
+node_t *rbtree_max(const rbtree *t)
+{
   // TODO: implement find
   return t->root;
 }
 
-int rbtree_erase(rbtree *t, node_t *p) {
+int rbtree_erase(rbtree *t, node_t *p)
+{
   // TODO: implement erase
   return 0;
 }
 
-int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
+int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n)
+{
   // TODO: implement to_array
   return 0;
 }
--- test/test-rbtree.c
+++ test/test-rbtree.c
@@ -369,7 +369,7 @@ void test_find_erase_rand(const size_t n, const unsigned int seed) {
 
 int main(void) {
   test_init();
-  // test_insert_single(1024);
+  test_insert_single(1024);
   // test_find_single(512, 1024);
   // test_erase_root(128);
   // test_find_erase_fixed();
